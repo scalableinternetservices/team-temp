9afe211611a13792e206a7d5842ef56b8a7245e7
AashitaP <harshupatwari@gmail.com>
2019-10-29 00:10:03 +0000

    added password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_000306) do
+ActiveRecord::Schema.define(version: 2019_10_29_000835) do
 
   create_table "answers", force: :cascade do |t|
     t.text "description"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2019_10_29_000306) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
