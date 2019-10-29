821d63e5e66c89eeabaec02ec2ec4600ec1045a7
AashitaP <harshupatwari@gmail.com>
2019-10-29 00:27:12 +0000

    added tests and relations between user and q&a

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: true
+  has_many :answers
+  has_many :questions
+  validates :username, presence: true, uniqueness: true, length: { maximum: 256 }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 end
