f80b56c21d56ca2e76bd3376a2ee3fff0216c32b
AashitaP <harshupatwari@gmail.com>
2019-10-29 00:06:38 +0000

    generated model and added validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_20_024800) do
+ActiveRecord::Schema.define(version: 2019_10_29_000306) do
 
   create_table "answers", force: :cascade do |t|
     t.text "description"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 2019_10_20_024800) do
 
   create_table "questions", force: :cascade do |t|
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
