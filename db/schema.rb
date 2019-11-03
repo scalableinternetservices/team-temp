118973c338a33627d337b1068df2f7a716ad4457
AashitaP <harshupatwari@gmail.com>
2019-11-03 20:35:30 +0000

    created migration to add category id to questions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_03_202440) do
+ActiveRecord::Schema.define(version: 2019_11_03_203312) do
 
   create_table "answers", force: :cascade do |t|
     t.text "description"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2019_11_03_202440) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.integer "category_id"
   end
 
   create_table "users", force: :cascade do |t|
