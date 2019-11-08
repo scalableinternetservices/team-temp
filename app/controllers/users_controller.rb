8edb9bb3a90cf254695cead69443ad253ca29167
icecubewang <wangfeilan@hotmail.com>
2019-11-08 06:57:08 +0000

    successful user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  private
+    def user_params
+      params.require(:user).permit(:username, :password, :password_confirmation)
+    end
+  
   def index
   end
   
