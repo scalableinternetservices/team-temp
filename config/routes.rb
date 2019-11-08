467bd8692aabd405e07b67252bb1348ad559b5c3
XILAI ZHANG <xilaizhang@XILAIs-MacBook-Pro.local>
2019-11-08 01:01:21 -0800

    problem with password_confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,12 @@ Rails.application.routes.draw do
   
   # Users Routes
   get '/signup', to: 'users#new'
-  get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show'
+  resources :users
   
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   
-  resources :users
   resources :answers
   resources :questions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
