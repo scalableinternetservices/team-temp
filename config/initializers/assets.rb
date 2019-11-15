71894d34ecda4f8ae00617d7eded424d4a072366
Langhao Zhong <zhong74.langhao@hotmail.com>
2019-11-15 04:16:06 +0000

    Fix Navbar to Top of Every Page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,3 +14,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w(static_page.scss)
 Rails.application.config.assets.precompile += %w(user_forms.scss)
+Rails.application.config.assets.precompile += %w(user_profile.scss)
+Rails.application.config.assets.precompile += %w(navbar.scss)
+
