d889c5754c342a0605b96b3a47be0a051dffa17d
Xilai Zhang <38773539+XilaiZhang@users.noreply.github.com>
2019-11-24 23:28:33 -0800

    fix empty problem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,6 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:username, :password, :password_confirmation)
+      params.fetch.(:user,{}).permit(:username, :password, :password_confirmation)
     end
 end
