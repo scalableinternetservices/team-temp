a47c54e094649e1e1247682540c75eb00f6ae0d3
Langhao Zhong <zhong74.langhao@hotmail.com>
2019-12-02 05:39:02 +0000

    Modify Puma Config

--- config/puma.rb
+++ config/puma.rb
@@ -11,6 +11,8 @@ threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 # threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
+preload_app!
+
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
 port        ENV.fetch("PORT") { 3000 }
