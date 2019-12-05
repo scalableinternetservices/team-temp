8c675892c77bcb161c7b5e8e8a61f307c4c5821c
Langhao Zhong <zhong74.langhao@hotmail.com>
2019-12-04 21:15:38 -0800

    Update static_page_controller.rb

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -4,7 +4,6 @@ class StaticPageController < ApplicationController
   def home
     @message = "Welcome to our study app!"
     #@questions = Question.all
-    #TODO-sql: .includes(...)
     @questions = Question.search(params[:search]).paginate(page: params[:page], per_page: 25)
     @session_loggedin = logged_in?
   end
