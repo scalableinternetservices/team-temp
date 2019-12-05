d03bae0d67177db9d1b2fffca4b3c5825929fb6c
Langhao Zhong <zhong74.langhao@hotmail.com>
2019-12-05 00:35:38 -0800

    Update static_page_controller.rb

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -6,5 +6,6 @@ class StaticPageController < ApplicationController
     #@questions = Question.all
     @questions = Question.paginate(page: params[:page], per_page: 25)
     @session_loggedin = logged_in?
+    @question_count = Rails.cache.fetch('question_count', expires_in: 1.minutes)
   end
 end
